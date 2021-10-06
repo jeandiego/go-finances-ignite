c00e7242a159d018e8ecc4b2e5e9e718e75689f9
Jean Diego <jeandiego.ds@gmail.com>
2021-10-06 14:39:52 -0300

    working on

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { HighlightCard } from "../../components/HighlightCard";
+import { TransactionCard, TransactionCardProps } from "../../components/TransactionCard";
 
 import {
   Container,
@@ -11,11 +12,53 @@ import {
   UserName,
   UserWrapper,
   Icon,
-  HighlightCards
+  HighlightCards,
+  Transactions,
+  Title,
+  TransactionsList,
 } from "./styles";
+
+export interface DataListProps extends TransactionCardProps{
+  id: string;
+}
 
 
 export function Dashboard() {
+  const data: DataListProps[] = [
+    {
+    id: "1",
+    type: "positive",
+    title: "Desenvolvimento de site",
+    amount: "R$ 12.000,00",
+    category: {
+      name: "Vendas",
+      icon: "dollar-sign"
+    },
+    date: "13/04/2020"
+  },
+    {
+    id: "2",
+    type: "negative",
+    title: "Aluguel do apartamento",
+    amount: "R$ 900,00",
+    category: {
+      name: "Alimentaçao",
+      icon: "coffee"
+    },
+    date: "13/04/2020"
+  },
+    {
+    id: "3",
+    type: "negative",
+    title: "Despesa qualquer",
+    amount: "R$ 1.200,00",
+    category: {
+      name: "Casa",
+      icon: "shopping-bag"
+    },
+    date: "13/04/2020"
+  },
+  ]
 
   return (
     <Container>
@@ -53,6 +96,16 @@ export function Dashboard() {
          />
 
       </HighlightCards>
+      <Transactions>
+        <Title>Listagem</Title>
+        <TransactionsList
+          data={data}
+          keyExtractor={(item) => item.id}
+          renderItem={({item})=>
+            <TransactionCard data={item}/>
+          }
+        />
+      </Transactions>
     </Container>
   )
 }
